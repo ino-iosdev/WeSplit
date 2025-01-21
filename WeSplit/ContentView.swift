6b884ef1369d989015d07f2be43fecf2ef549869
ino yang popper <ino.yang.p@gmail.com>
2025-01-20 22:40:03 -0800

    finished app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,24 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let localCurrency = Locale.current.currency?.identifier ?? "USD"
+    
+    var totalPerPerson: Double {
+        grandTotal / Double(numberOfPeople + 2)
+    }
+    
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount * tipSelection / 100
+        return checkAmount + tipValue
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
                 Section {
-                    Text("Hello Jeff!")
+                    TextField("Amount", value: $checkAmount, format: .currency(code: localCurrency))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
                 }
-                Section {
-                    Text("Hello Jeff!")
-                    Text("Hello Jeff!")
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
                 }
-                Section {
-                    Text("Hello Jeff!")
-                    Text("Hello Jeff!")
-                    Text("Hello Jeff!")
+                
+                Section("Total amount") {
+                    Text(grandTotal, format: .currency(code: localCurrency))
+                }
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: localCurrency))
                 }
             }
-            .navigationTitle("Greeting to Jeff")
+            .navigationTitle("WeSplit")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
